8fc63fbf3469354a173417d24398a27e77dc078b
Samuel Keullen <sk.caragua2017@gmail.com>
2022-05-24 12:05:55 -0300

    Sistema de logs adicionado

--- Game.py
+++ Game.py
@@ -3,6 +3,10 @@ import Player as Player
 import Dice as Dice
 import time
 import random
+import datetime
+
+log = open("log.txt", "w");
+
 
 def loading():
     time.sleep(0.5);
@@ -13,28 +17,38 @@ def round(ordem_jogadores, dice, gameArea):
     while count < 4:
         loading();
         dado_sorteado = dice.rollDice();
-        print("-------------------------------------------------------------------------------------------------");
-        print("O jogador {} tirou o n° {}.".format(ordem_jogadores[count],dado_sorteado));
+        print("-------------------------------------------------------------------------------------------------", file=log);
+        print("O jogador {} tirou o n {}.".format(ordem_jogadores[count],dado_sorteado), file=log);
 
         go_ahead = 0 + dado_sorteado;
 
         if gameArea.arr_casas[go_ahead] == 0:
-            print("\nO jogador {} caiu em uma casa sem propriedades para adquirir".format(ordem_jogadores[count]))
+            print("\nO jogador {} caiu em uma casa sem propriedades para adquirir".format(ordem_jogadores[count]), file=log)
         elif gameArea.arr_casas[go_ahead] == 1:
-            print("\nO jogador {} comprou a propriedade na casa: {}".format(ordem_jogadores[count], go_ahead))
+            print("\nO jogador {} comprou a propriedade na casa: {}".format(ordem_jogadores[count], go_ahead), file=log)
         elif gameArea.arr_casas[go_ahead] == 2:
-            print("\nO jogador {} caiu na casa {}, anteriormente comprada, deverá pagar aluguel.".format(ordem_jogadores[count], go_ahead))
+            print("\nO jogador {} caiu na casa {}, anteriormente comprada, devera pagar aluguel.".format(ordem_jogadores[count], go_ahead), file=log)
 
         count+= 1
-    print("-------------------------------------------------------------------------------------------------");
+    print("-------------------------------------------------------------------------------------------------", file=log);
     
 
 
 def main():
-    print("Iniciando módulos...\n");
+
+    print("Iniciando o jogo...")
+    print("Por favor, veja o arquivo 'log.txt' caso queira rever o andamento do jogo.")
+
+    #inicio = time.time()
+    begin_time = datetime.datetime.now()
+
+    print("Iniciando o jogo...", file=log)
+    print("Por favor, veja o arquivo 'log.txt' caso queira rever o andamento do jogo.", file=log)
+    
+    print("Iniciando modulos...\n", file=log);
     loading();
 
-    print("Gerando tabuleiro...\n");
+    print("Gerando tabuleiro...\n", file=log);
     
     loading();
     #Criando objeto do tabuleiro...;
@@ -47,46 +61,46 @@ def main():
     loading();
     num_casas_tabuleiro = gameArea.arr_casas.count(0)+gameArea.arr_casas.count(1);
     
-    print("Tabuleiro gerado!\n");
+    print("Tabuleiro gerado!\n", file=log);
 
     loading();
-    print("Informações do tabuleiro:\nN° de casas no tabuleiro: {}\nN° de propriedades:{}\nN° de propiedades compradas: {}\n".format(num_casas_tabuleiro,gameArea.n_propriedades, gameArea.n_prop_compradas));
+    print("Informacoes do tabuleiro:\nN de casas no tabuleiro: {}\nN de propriedades:{}\nN de propiedades compradas: {}\n".format(num_casas_tabuleiro,gameArea.n_propriedades, gameArea.n_prop_compradas), file=log);
 
     loading();
     loading();
 
-    print("Conectando jogadores...\n");
+    print("Conectando jogadores...\n", file=log);
 
     loading();
 
-    print("Jogador 1 encontrado!\n");
+    print("Jogador 1 encontrado!\n", file=log);
     j1 = Player.Player(1, 300, 0);
 
     loading();
-    print("Jogador 2 encontrado!\n");
+    print("Jogador 2 encontrado!\n", file=log);
     j2 = Player.Player(2, 300, 0);
 
     loading();
-    print("Jogador 3 encontrado!\n");
+    print("Jogador 3 encontrado!\n", file=log);
     j3 = Player.Player(3, 300, 0);
 
     loading();
-    print("Jogador 4 encontrado!\n");
+    print("Jogador 4 encontrado!\n", file=log);
     j4 = Player.Player(4, 300, 0);
 
-    print("Exibindo informações dos jogadores:\n");
-    print("-------------------------------------------------------------------------------------------------");
-    print("Jogador {}:\nSaldo: {}\nN° de voltas no tabuleiro: {}".format(j1.numJogador, j1.saldo, j1.voltas));
-    print("-------------------------------------------------------------------------------------------------");
-    print("Jogador {}:\nSaldo: {}\nN° de voltas no tabuleiro: {}".format(j2.numJogador, j2.saldo, j2.voltas));
-    print("-------------------------------------------------------------------------------------------------");
-    print("Jogador {}:\nSaldo: {}\nN° de voltas no tabuleiro: {}".format(j3.numJogador, j3.saldo, j3.voltas));
-    print("-------------------------------------------------------------------------------------------------");
-    print("Jogador {}:\nSaldo: {}\nN° de voltas no tabuleiro: {}".format(j4.numJogador, j4.saldo, j4.voltas));
-    print("-------------------------------------------------------------------------------------------------");
+    print("Exibindo informacoes dos jogadores:\n", file=log);
+    print("-------------------------------------------------------------------------------------------------", file=log);
+    print("Jogador {}:\nSaldo: {}\nN de voltas no tabuleiro: {}".format(j1.numJogador, j1.saldo, j1.voltas), file=log);
+    print("-------------------------------------------------------------------------------------------------", file=log);
+    print("Jogador {}:\nSaldo: {}\nN de voltas no tabuleiro: {}".format(j2.numJogador, j2.saldo, j2.voltas), file=log);
+    print("-------------------------------------------------------------------------------------------------", file=log);
+    print("Jogador {}:\nSaldo: {}\nN de voltas no tabuleiro: {}".format(j3.numJogador, j3.saldo, j3.voltas), file=log);
+    print("-------------------------------------------------------------------------------------------------", file=log);
+    print("Jogador {}:\nSaldo: {}\nN de voltas no tabuleiro: {}".format(j4.numJogador, j4.saldo, j4.voltas), file=log);
+    print("-------------------------------------------------------------------------------------------------", file=log);
 
     loading();
-    print("Gerando o dado...\n");
+    print("Gerando o dado...\n", file=log);
 
     #criando objeto do Dado;
     dice = Dice.Dice(0, []);
@@ -96,11 +110,11 @@ def main():
 
     loading();
 
-    print("Os lados do dado:\n{}\n".format(dice.arrayNum));
+    print("Os lados do dado:\n{}\n".format(dice.arrayNum), file=log);
 
     loading();
 
-    print("Reordenando jogadores em uma ordem aleatória...\n");
+    print("Reordenando jogadores em uma ordem aleatoria...\n", file=log);
 
     loading();
 
@@ -108,21 +122,22 @@ def main():
     array_players = list(array_numbers);
     random.shuffle(array_players)
 
-    print("E a ordem de jogada dos jogadores será:\n{}".format(array_players));
+    print("E a ordem de jogada dos jogadores sera:\n{}".format(array_players), file=log);
 
-    print("Iniciando partida...\n");
+    print("Iniciando partida...\n", file=log);
 
     loading();
 
-    print("Rolando dados...\n");
+    print("Rolando dados...\n", file=log);
     round(array_players, dice, gameArea);
 
     loading();
 
+    print("\n\nTempo decorrido(HH:MM:SS:MS):",file=log)
+    print(datetime.datetime.now() - begin_time, file=log)
 
-    
-
-
+    print("\n\nTempo decorrido(HH:MM:SS:MS):")
+    print(datetime.datetime.now() - begin_time)
 
 
 if __name__ == "__main__":
